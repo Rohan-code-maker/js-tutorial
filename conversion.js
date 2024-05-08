dbd2fec1b618510b40133f492f03109fd09ba323
Rohan Kumar Gupta <116427952+Rohan-code-maker@users.noreply.github.com>
2024-05-08 16:31:39 +0530

    Conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,21 @@
+let score = undefined
+//console.log(typeof(score))
+
+let newScore = Number(score)
+// console.log(typeof(newScore))
+// console.log(newScore)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "Rohan"
+
+let  newLoggedIn = Boolean(isLoggedIn)
+console.log(newLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Rohan" => true
