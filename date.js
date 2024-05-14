84708968a6f3f357c29246a72eb518a21e9d6e79
Rohan Kumar Gupta <116427952+Rohan-code-maker@users.noreply.github.com>
2024-05-14 18:45:08 +0530

    learnt about Date Library

---
+++ date.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+// let newDate = new Date(2023,0,3,5,7,8)
+let newDate = new Date('02-04-2023')
+// console.log(newDate.toLocaleString())
+// console.log(newDate.toString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(newDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
