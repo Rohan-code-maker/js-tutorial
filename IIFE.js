7f3fe2ec9559d27ae3495809976b344994bc95ab
Rohan Kumar Gupta <116427952+Rohan-code-maker@users.noreply.github.com>
2024-05-23 16:53:15 +0530

    added truthy and false values also Nullish and ternary operator

--- IIFE.js
+++ IIFE.js
@@ -8,3 +8,25 @@
 ( (user) =>{
     console.log(`Hello ${user}`)
 })("Rohan");
+
+//falsy values
+// false, 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", "false", " ", [], {}, function(){}
+
+// Nullish Coalescing Operator (??): null, undefined
+
+let val1
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1)
+
+// Ternary operator
+// (consdition) ? true : false
+
+const price = 100
+price <=80 ? console.log("less than 80") : console.log("more than 80")
